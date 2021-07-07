2039119565ba32124e0ac1020cc0784185dc5b7f
Domas <domasx2@gmail.com>
2021-07-07 13:49:24 +0300

    test for detecting editable-ness of a rules datasource

--- public/app/features/alerting/unified/mocks.ts
+++ public/app/features/alerting/unified/mocks.ts
@@ -11,7 +11,10 @@ import {
 
 let nextDataSourceId = 1;
 
-export const mockDataSource = (partial: Partial<DataSourceInstanceSettings> = {}): DataSourceInstanceSettings => {
+export const mockDataSource = (
+  partial: Partial<DataSourceInstanceSettings> = {},
+  meta: Partial<DataSourcePluginMeta> = {}
+): DataSourceInstanceSettings => {
   const id = partial.id ?? nextDataSourceId++;
 
   return {
@@ -27,6 +30,7 @@ export const mockDataSource = (partial: Partial<DataSourceInstanceSettings> = {}
           large: 'https://prometheus.io/assets/prometheus_logo_grey.svg',
         },
       },
+      ...meta,
     } as any) as DataSourcePluginMeta,
     ...partial,
   };
